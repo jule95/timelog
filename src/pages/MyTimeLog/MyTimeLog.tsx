0bb3c23fcb9d1d8eb9fc16e8be1cf77529d531ed
Julius <julius.plass@hotmail.com>
2024-11-25 14:33:14 +0100

    * Finished basic MyTimeLog page.

--- src/pages/MyTimeLog/MyTimeLog.tsx
+++ src/pages/MyTimeLog/MyTimeLog.tsx
@@ -1,5 +1,67 @@
-const MyTimeLog = () => (
-  <div>[MYTIMELOG]</div>
-);
+import { useTranslation } from 'react-i18next';
+import { useQuery } from '@apollo/client';
+import { IStaffResponse, ITimeLogResponse } from '../../common/interfaces/api.interfaces.ts';
+import { LOAD_STAFF, LOAD_TIME_LOGS } from '../../api/Queries.ts';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { CircularProgress, Paper, Table, TableBody, TableCell, TableContainer, TableRow } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { IMyTimeLogState } from './MyTimeLog.types.ts';
 
+const MyTimeLog = () => {
+  const { t } = useTranslation();
+  const { data: timeLogData, loading: timeLogLoading } = useQuery<ITimeLogResponse>(LOAD_TIME_LOGS);
+  const { data: staffData, loading: staffLoading } = useQuery<IStaffResponse>(LOAD_STAFF);
+  const [state, setState] = useState<IMyTimeLogState[]>([]);
+
+
+  useEffect(() => {
+    if (!timeLogData || !staffData) {
+      return;
+    }
+
+    setState(timeLogData.timeLogs.map(item => {
+      return {
+        id: item.id,
+        day: item.day,
+        hours: item.hours,
+        project: item.project_name,
+        name: staffData.staff.find(staffItem => staffItem.id === item.staff_id)?.name ?? `-`,
+      }
+    }))
+
+    console.log(timeLogData, staffData);
+  }, [timeLogData, staffData]);
+
+  return (
+    <div>
+      <Typography variant="h4">
+        {t(`navbar.myTimeLog`)}
+      </Typography>
+
+      { (timeLogLoading || staffLoading) && (
+        <Box sx={{ display: 'flex' }}>
+          <CircularProgress />
+        </Box>
+      )}
+
+      { timeLogData && (
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableBody>
+              {state.map(row => (
+                <TableRow key={row.id}>
+                  <TableCell>{row.day}</TableCell>
+                  <TableCell>{row.hours}</TableCell>
+                  <TableCell>{row.name}</TableCell>
+                  <TableCell>{row.project}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    </div>
+  )
+}
 export default MyTimeLog;
---
+++ src/pages/MyTimeLog/MyTimeLog.types.ts
@@ -0,0 +1,7 @@
+export interface IMyTimeLogState {
+  id: number;
+  day: string;
+  hours: number;
+  name: string;
+  project: string;
+}
