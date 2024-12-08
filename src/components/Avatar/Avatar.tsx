603f1d6ddc93bc69f1d4d370956be4a70885137a
Julius <julius.plass@hotmail.com>
2024-12-08 16:51:38 +0100

    * Small Fix

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -4,10 +4,12 @@ import { FC, useContext } from 'react';
 import AppContext from '../../state/app-context.ts';
 import { Button } from 'primereact/button';
 import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router-dom';
 
 const Avatar: FC = () => {
   const { t } = useTranslation();
   const { actions } = useContext(AppContext);
+  const location = useLocation();
 
   const handleClick = () => {
     actions.toggleNewEntry();
@@ -17,13 +19,12 @@ const Avatar: FC = () => {
     <div
       className="Avatar"
       style={{ backgroundImage: `url(${avatar})` }}>
-      {location.pathname && (
-        <Button
-          className="Avatar__button"
-          onClick={handleClick}>
-          {t(`app.newEntry`)}
-        </Button>
-      )}
+      <Button
+        className="Avatar__button"
+        visible={location.pathname === `/myTimeLog`}
+        onClick={handleClick}>
+        {t(`app.newEntry`)}
+      </Button>
     </div>
   );
 };
