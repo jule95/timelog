225d2c27bbd747df9167617066818ac0197d2f45
Julius <julius.plass@hotmail.com>
2024-12-08 13:28:32 +0100

    * Finished basic Navbar component.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,54 +1,38 @@
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import config from '../../config.ts';
+import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { Menubar } from 'primereact/menubar';
 import './Navbar.scss';
-import { ButtonBase } from '@mui/material';
 
-const Navbar = () => {
+const Navbar: FC = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const items = [
+    {
+      command: () => { navigate(`/myTimeLog`); },
+      label: t(`navbar.myTimeLog`),
+    },
+    {
+      command: () => { navigate(`/employees`); },
+      label: t(`navbar.employees`),
+    },
+    {
+      command: () => { navigate(`/projects`); },
+      label: t(`navbar.projects`),
+    },
+    {
+      command: () => { navigate(`/`); },
+      id: `navbar-menu-item-home`,
+      label: t(`navbar.home`),
+    },
+  ];
 
   return (
-    <AppBar
+    <Menubar
       className="Navbar"
-      elevation={0}>
-      <Toolbar className="Navbar__toolbar">
-        <Box>
-          <Typography className="Navbar__toolbar-title">
-            {t(`navbar.timeLog`)}
-          </Typography>
-        </Box>
-
-        <Box>
-          <ButtonBase
-            disableRipple
-            className="Navbar__toolbar-button"
-            href={config.routes.myTimeLog}>
-            {t(`navbar.myTimeLog`)}
-          </ButtonBase>
-          <ButtonBase
-            disableRipple
-            className="Navbar__toolbar-button"
-            href={config.routes.employees}>
-            {t(`navbar.employees`)}
-          </ButtonBase>
-          <ButtonBase
-            disableRipple
-            className="Navbar__toolbar-button"
-            href={config.routes.projects}>
-            {t(`navbar.projects`)}
-          </ButtonBase>
-          <ButtonBase
-            disableRipple
-            className="Navbar__toolbar-button Navbar__toolbar-button--home"
-            href={config.routes.home}>
-            {t(`navbar.home`)}
-          </ButtonBase>
-        </Box>
-      </Toolbar>
-    </AppBar>
+      model={items}
+      start={<h1 className="Navbar__logo">{t(`navbar.timeLog`)}</h1>} />
   );
 };
 
