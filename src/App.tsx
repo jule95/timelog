64a15c713a6206bacd1aa8b0911c926a639886d0
Julius <julius.plass@hotmail.com>
2024-12-08 18:38:48 +0100

    * Added theme. * Markup changes for App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import './App.scss';
 import { Outlet } from 'react-router-dom';
 import './App.scss';
-import Navbar from './components/Navbar/Navbar.tsx';
-import { Container } from '@mui/material';
-import Toolbar from '@mui/material/Toolbar';
 import Footer from './components/Footer/Footer.tsx';
 import Avatar from './components/Avatar/Avatar.tsx';
+import { PrimeReactProvider } from 'primereact/api';
+import Navbar from './components/Navbar/Navbar.tsx';
 
 const App = () => (
   <div className="App">
-    <Navbar />
-    <Toolbar className="App__toolbar" />
-    <Container className="App__container">
-      <Outlet />
-      <Avatar />
+    <PrimeReactProvider>
+      <Navbar />
+      <div className="App__content">
+        <Outlet />
+        <Avatar />
+      </div>
       <Footer />
-    </Container>
+    </PrimeReactProvider>
   </div>
 );
-
 export default App;
