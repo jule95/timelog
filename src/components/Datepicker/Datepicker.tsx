a3bf05e54431a41bf13b5431bef65cb51f2252a9
Julius <julius.plass@hotmail.com>
2024-12-09 12:10:05 +0100

    * WIP - Basic Datepicker component.

---
+++ src/components/Datepicker/Datepicker.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import './Datepicker.scss';
+import { IDatepickerProps } from './Datepicker.types.ts';
+
+const Datepicker: FC<IDatepickerProps> = props => {
+  return (
+    <div className="Datepicker">
+      [DATEPICKER]
+    </div>
+  );
+};
+
+export default Datepicker;
